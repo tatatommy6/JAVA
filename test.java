490bcdc4e8adffc214aea666b2c88f6cc67cd81d
tatatommy6 <tatatommy6@naver.com>
2023-11-12 18:24:30 +0900

    java 9장 9.7 gridlayout 배치관리자

--- test.java
+++ test.java
@@ -2,15 +2,21 @@ import javax.swing.*;
 import java.awt.*;
 public class test extends JFrame{
     public test(){
-        setTitle("borderlayout sample");
+        setTitle("gridlayout sample");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        GridLayout gr=new GridLayout(4,2);
+        gr.setVgap(5);
         Container c=getContentPane();
-        c.setLayout(new BorderLayout(30,20));
-        c.add(new JButton("Calculate"),BorderLayout.CENTER);
-        c.add(new JButton("add"),BorderLayout.NORTH);
-        c.add(new JButton("sub"),BorderLayout.SOUTH);
-        c.add(new JButton("nul"),BorderLayout.EAST);
-        c.add(new JButton("div"),BorderLayout.WEST);
+        c.setLayout(gr);
+        c.add(new JLabel("이름"));
+        c.add(new JTextField(""));
+        c.add(new JLabel("학번"));
+        c.add(new JTextField(""));
+        c.add(new JLabel("학과"));
+        c.add(new JTextField(""));
+        c.add(new JLabel("과목"));
+        c.add(new JTextField(""));
+
         setSize(300,200);
         setVisible(true);
     }
